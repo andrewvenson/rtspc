8d468afad00443f314315e6fa962fe0ef79ffb5e
Andrew Venson <vensoneandrew@gmail.com>
2025-01-12 18:32:12 -0600

    some simple fe

--- stream_server/index.js
+++ stream_server/index.js
@@ -1,8 +1,9 @@
+const https = require("https");
+const fs = require("fs");
 const path = require('node:path');
 const express = require('express');
 const app = express();
-
-const port = 3008;
+const HTTPS_PORT = 8443;
 
 app.use('/public', express.static(path.join(__dirname, 'public'), {
 	setHeaders: (res, filePath) => {
@@ -21,6 +22,11 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '/index.html'));
 })
 
-app.listen(port, () => {
-	console.log("Listening on port", port);
-})
+const options = {
+	key: fs.readFileSync(path.join(__dirname, 'key.pem')),
+	cert: fs.readFileSync(path.join(__dirname, 'cert.pem')),
+};
+
+https.createServer(options, app).listen(HTTPS_PORT, () => {
+	console.log(`Server running at https://localhost/${HTTPS_PORT}`);
+});
